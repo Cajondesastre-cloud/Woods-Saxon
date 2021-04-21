2eda934a0eda4e52220d6f7b99dff9e36e5f0e94
Cajondesastre-cloud <81261522+Cajondesastre-cloud@users.noreply.github.com>
2021-04-21 11:36:53 +0200

    21/4/21 Ya devuelve todos los resultados para l = 0.

--- ProgramaCompleto.py
+++ ProgramaCompleto.py
@@ -46,13 +46,13 @@ def norm(array, h):
 def Runge4(h,f,g,w,u,r, E, tipo): # Método de Runge-Kutta Orden 4 para ec. dif 2º ord.
     
     k1 = h*f(w,u,r,E)
-    k1_g = h*g(w,u,r, E)
+    k1_g = h*g(w,u,r, E, tipo)
     k2 = h*f(w +1/2*k1_g, u +1/2*k1, r +1/2*h, E)
-    k2_g = h*g(w +1/2*k1_g, u +1/2*k1, r +1/2*h, E)
+    k2_g = h*g(w +1/2*k1_g, u +1/2*k1, r +1/2*h, E, tipo)
     k3 = h*f(w + 1/2*k2_g, u + 1/2*k2, r +1/2*h, E)
-    k3_g = h*g(w + 1/2*k2_g, u + 1/2*k2, r +1/2*h, E)
+    k3_g = h*g(w + 1/2*k2_g, u + 1/2*k2, r +1/2*h, E, tipo)
     k4 = h*f(w +k3_g, u +k3, r+h, E)
-    k4_g = h*g(w +k3_g, u +k3, r+h, E)
+    k4_g = h*g(w +k3_g, u +k3, r+h, E, tipo)
     u2 = u + 1/6*(k1+2*k2+2*k3+k4)
     w2 = w + 1/6*(k1_g+2*k2_g+2*k3_g+k4_g)
     return w2, u2
@@ -60,20 +60,20 @@ def Runge4(h,f,g,w,u,r, E, tipo): # Método de Runge-Kutta Orden 4 para ec. dif 
 def Runge2(h,f,g,w,u,r, E, tipo): # Método de Runge-Kutta Orden 2 para ec. dif 2º ord.
     
     k1 = h*f(w,u,r,E)
-    k1_g = h*g(w,u,r, E)
+    k1_g = h*g(w,u,r, E, tipo)
     k2 = h*f(w +1/2*k1_g, u +1/2*k1, r +1/2*h, E)
-    k2_g = h*g(w +1/2*k1_g, u +1/2*k1, r +1/2*h, E)
+    k2_g = h*g(w +1/2*k1_g, u +1/2*k1, r +1/2*h, E, tipo)
     u2 = u + k2
     w2 = w + k2_g
     return w2, u2
 
 def Leap(h,f,r,t,E,i, xapoyo, tipo):    #Método Leapfrog para ec. dif 2º ord.
     if i == 0:
-        x12 = r+h/2*f(r,t, E)
+        x12 = r+h/2*f(r,t, E, tipo)
     elif i != 0:
         x12 = xapoyo
-    x2 = r + h*f(x12, t+1/2*h, E)
-    x32 = x12 + h*f(x2, t+h, E)
+    x2 = r + h*f(x12, t+1/2*h, E, tipo)
+    x32 = x12 + h*f(x2, t+h, E, tipo)
     xapoyo = x32
     return x2, xapoyo
 
@@ -161,7 +161,7 @@ if menu == "s":
     
         selec = int(input("Elija el método para la resolución de la ecuación \
 de Schrödinger:\n 1. Runge-Kutta 4º orden. \n\
-         2. Runge-Kutta 2º orden. \n 3. Leapfrog. \n 4. Euler. \n"))
+ 2. Runge-Kutta 2º orden. \n 3. Leapfrog. \n 4. Euler. \n"))
          
         if selec == 1:
             func = Runge4
@@ -173,16 +173,13 @@ de Schrödinger:\n 1. Runge-Kutta 4º orden. \n\
             func = Euler
         
         (n, l) = (int(input("Número cuántico principal: \n")), 
-                  int(input("Número cuántico azimutal: \n ")))
+                  int(input("Número cuántico azimutal: \n")))
         
-        if n >= l:
-            factor = n-l
-        elif l > n:
-            factor = l-n +1
+        factor = n
         
         if l == 0:
             tipo = int(input("¿Cuál versión del potencial de Woods-Saxon? \
-\n 1. Clásico \n 2. Modificado. \n"))
+\n 1. Clásico \n 2. Generalizado. \n"))
         
         if l != 0:
             app = int(input("¿Realizar aproximación o tomar r cerca de 0? \
@@ -195,18 +192,19 @@ de Schrödinger:\n 1. Runge-Kutta 4º orden. \n\
 \n 3. Clásico \n 5. Modificado. \n "))
             
         if tipo == 3 or tipo == 5:
-            x = np.linspace(1e-5, R + 15, 1000)      # Array de posiciones.
+            x = np.linspace(1e-5, R + Rn, 1000)      # Array de posiciones.
+            dx = x[1] - x[0]                         # Diferencial de x.
         
         if tipo == 5 or tipo == 6 or tipo == 2:
             W0 = float(input("Introducir profundidad del pozo, W0 (float): \n "))
             
         met = int(input("¿Cuál método de búsqueda de autovalores? \
-\n 1. Bipartición. \n 2. Newton. \n"))
+\n 1. Bipartición. \n"))
         
-        if met == 1:                                # Resolviendo por bipartición.
+        if met == 1:                 # Resolviendo por bipartición.
             Nmax = 100
             prec = 1e-15
-            if (n, l) == (0,0):
+            if (n, l) == (0,0):      # Cotas de la energía para cada estado.
                 Emax = -30
                 Emin = -40
             elif (n, l) == (1, 0):
@@ -217,20 +215,20 @@ de Schrödinger:\n 1. Runge-Kutta 4º orden. \n\
                 Emin = -2  
             Rn = 15   
             En = (Emax + Emin)/2
-            dE0 = En
+            dE0 = abs(En)
             
-            if selec == 1 and selec != 3:
+            if selec != 3:
                 c = 0                           # Contador
                 while dE0 > prec:
                     c += 1
                     if c == Nmax: 
                         break
                     u0 = 0                      # Condición inicial para r*R(r)
-                    w0 = 1e-5*(-1)**(factor)    # Condición inicial para du/dr
+                    w0 = 1e-5*(-1)**(factor+2)  # Condición inicial para du/dr
                     ur = []
                     wr = []
-                    ur(u0)
-                    wr(w0)
+                    ur.append(u0)
+                    wr.append(w0)
                     for i in range(len(x)-1):
                         sol = func(dx,f1, f2 ,wr[i], ur[i], x[i], En, tipo)
                         ur.append(sol[1])
@@ -240,15 +238,15 @@ de Schrödinger:\n 1. Runge-Kutta 4º orden. \n\
                                 Emin = En
                                 En = Emin + (Emax-Emin)/2
                                 break
-                            elif ur[i+1] < 0:
+                            elif ur[-1] < 0:
                                 Emax = En
                                 En = Emax - (Emax-Emin)/2
                                 break
                     dE0 = abs(Emax-Emin)
                     
                 print(En)
-            
-            if selec == 1 and selec == 3:
+                
+            if selec == 3:
                 c = 0                           # Contador
                 while dE0 > prec:
                     c += 1
@@ -260,7 +258,7 @@ de Schrödinger:\n 1. Runge-Kutta 4º orden. \n\
                     oscleap.append([u0,w0])
                     roscapoyo = [0,0]
                     for i in range(len(x)-1):
-                        oscleap.append(Leap(dx,func,oscleap[i],x[i], En, i, roscapoyo, tipo)[0])
+                        oscleap.append(Leap(dx,f,oscleap[i],x[i], En, i, roscapoyo, tipo)[0])
                         roscapoyo = Leap(dx,f,oscleap[i], x[i],En, i, roscapoyo, tipo)[1]
                         if i == len(x)-2:
                             if oscleap[-1][0] > 0:
@@ -274,27 +272,24 @@ de Schrödinger:\n 1. Runge-Kutta 4º orden. \n\
                         dE0 = abs(Emax-Emin)
     
                 print(En)
-        
-        if met == 2:
-            Nmax = 100
-            prec = 1e-15
-            if (n, l) == (0,0):
-                En = -38.2
-                Eap = -38.2   
-                dE0 = 1
-                deltaE = 1e-4
-                
+              
         rep = input("¿Quiere representar los resultados (s/n)?\n")
         
         if rep == "s":
+            
+            sav = input("¿Quiere guardar los resultados (s/n)?\n")
+            
             plt.title("U(r) frente a la coordenada radial")
             plt.xlabel("r (fm)")
             plt.ylabel("U(r)")
             urn = norm(ur, dx)
             plt.plot(x,urn, color ="k", label="n = {}". format(n))
             plt.legend(loc="best", frameon = False)
+            plt.show()
             
-
+            if sav == "s":
+                plt.savefig(input("Introduzca el nombre de la figura 1"), dpi= 400)
+                
             fig2 = plt.figure()
             plt.title("R(r) frente a la coordenada radial")
             plt.xlabel("r (fm)")
@@ -304,6 +299,9 @@ de Schrödinger:\n 1. Runge-Kutta 4º orden. \n\
             rrn = norm(rr, dx)
             plt.plot(x[1:], rrn[1:], color="k", label="n = {}". format(n))
             plt.legend(loc="best", frameon = False)
+                 
+            if sav == "s":
+                plt.savefig(input("Introduzca el nombre de la figura 2"), dpi= 400)
         
         fin = input("¿Quiere seguir haciendo simulaciones (s/n)?\n")
         
@@ -317,3 +315,14 @@ de Schrödinger:\n 1. Runge-Kutta 4º orden. \n\
 ##############################################################################
 ##############################################################################
 ##############################################################################
+##############################################################################
+##############################################################################
+##############################################################################
+#######################################################
+#######################################################
+#############################################
+#############################################
+
+# Ejecución rápida, se le pasa un array con las opciones.
+
+#if menu == "n":
