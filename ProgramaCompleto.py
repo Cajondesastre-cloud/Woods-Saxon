9676b0d5bc488121d96817c8e07e3fb591bcc6ab
Cajondesastre-cloud <81261522+Cajondesastre-cloud@users.noreply.github.com>
2021-06-07 20:03:25 +0200

    Add files via upload
    Versión con inicio rápido, 7/6/21

--- ProgramaCompleto.py
+++ ProgramaCompleto.py
@@ -266,7 +266,7 @@ de Schrödinger:\n 1. Runge-Kutta 4º orden. \n\
                                 break
                         dE0 = abs(Emax-Emin)
     
-                print(En)
+                print(En, "MeV")
                 ur = []
                 wr = []
 
@@ -326,4 +326,169 @@ de Schrödinger:\n 1. Runge-Kutta 4º orden. \n\
 
 # Ejecución rápida, se le pasa un array con las opciones.
 
-#if menu == "n":
+elif menu == "n":
+    acabar = True
+    print("El formato del archivo de datos a cargar será como el que se incluye como ejemplo.")
+    print(" Array ejemplo: 1 0 0 1 1 1 0 50 usando espacios como separador")
+    print(" 1º = método (1-4), 2º y 3º = (n,l), 4º potencial clásico o generalizado (1-2),\
+ 5º = prueba y error (1), 6º representar? (0 = No, 1 = Sí), 7º Guardar (0-1) y 8º W0 en caso\
+ de que el potencial sea generalizado.")
+    conf = np.loadtxt(input(" Nombre del archivo a cargar (incluyendo extensión, .txt o .dat preferentemente): \n"))
+    selec = conf[0]
+    (n, l) = (conf[1], conf[2])
+    tipo = conf[3]
+    met = conf[4]
+    rep = "n"
+    if conf[5] == 1:
+        rep = "s"
+    sav = "n"
+    if conf[6] == 1:
+        sav = "s"
+    W0 = conf[7]
+    
+    if selec == 1:
+        func = Runge4
+        metnam = "Runge-Kutta 4º orden"
+    elif selec == 2:
+        func = Runge2
+        metnam = "Runge-Kutta 2º orden"
+    elif selec == 3:
+        func = Leap
+        metnam = "Leapfrog"
+    elif selec == 4:
+        func = Euler
+        metnam = "Euler"
+        
+    if l != 0:
+        if tipo == 1:
+            tipo = 3
+        if tipo == 2:
+            tipo == 4
+            
+    if tipo == 3 or tipo == 4:
+        x = np.linspace(1e-5, R + Rn, 1000)      # Array de posiciones.
+        dx = x[1] - x[0]                         # Diferencial de x.
+        
+     
+    if met == 1:                 # Resolviendo por bipartición.
+        Nmax = 100               # Número máximo de iteraciones.
+        prec = 1e-15             # Preción máxima requerida.
+        if (n, l) == (0,0):      # Cotas de la energía para cada estado.
+            Emax = -30
+            Emin = -40
+        elif (n, l) == (1, 0):
+            Emax = -15
+            Emin = -20
+        elif (n, l) == (2, 0):
+            Emax = -0
+            Emin = -2  
+        elif (n, l) == (0,1):
+            Emax = -30
+            Emin = -35
+        elif (n, l) == (0,2):
+            Emax = -20
+            Emin = -30
+        elif (n, l) == (1,1):
+            Emax = -10
+            Emin = -15
+        elif (n, l) == (1,2):
+            Emax = -1
+            Emin = -7   
+        Rn = 15   
+        En = (Emax + Emin)/2
+        dE0 = abs(En)
+            
+        if selec != 3:
+            c = 0                           # Contador
+            while dE0 > prec:
+                c += 1
+                if c == Nmax: 
+                    break
+                u0 = 0                      # Condición inicial para r*R(r)
+                w0 = 1e-5*(-1)**(n)         # Condición inicial para du/dr
+                ur = []
+                wr = []
+                ur.append(u0)
+                wr.append(w0)
+                for i in range(len(x)-1):
+                    sol = func(dx,f1, f2 ,wr[i], ur[i], x[i], En, tipo)
+                    ur.append(sol[1])
+                    wr.append(sol[0])
+                    if i == len(x)-2:
+                        if ur[-1] > 0:
+                            Emin = En
+                            En = Emin + (Emax-Emin)/2
+                            break
+                        elif ur[-1] < 0:
+                            Emax = En
+                            En = Emax - (Emax-Emin)/2
+                            break
+                dE0 = abs(Emax-Emin)
+                    
+            print(En, "MeV")
+                
+        if selec == 3:
+            c = 0                           # Contador
+            while dE0 > prec:
+                c += 1
+                if c == Nmax: 
+                    break
+                u0 = 0                       # Condición inicial para r*R(r)
+                w0 = 1e-5*(-1)**(n)          # Condición inicial para du/dr
+                oscleap = []
+                oscleap.append([u0,w0])
+                roscapoyo = [0,0]
+                for i in range(len(x)-1):
+                    oscleap.append(Leap(dx,f,oscleap[i],x[i], En, i, roscapoyo, tipo)[0])
+                    roscapoyo = Leap(dx,f,oscleap[i], x[i],En, i, roscapoyo, tipo)[1]
+                    if i == len(x)-2:
+                        if oscleap[-1][0] > 0:
+                            Emin = En
+                            En = Emin + (Emax-Emin)/2
+                            break
+                        elif oscleap[-1][0] < 0:
+                            Emax = En
+                            En = Emax - (Emax-Emin)/2
+                            break
+                    dE0 = abs(Emax-Emin)
+    
+            print(En, "MeV")
+            ur = []
+            wr = []
+
+            for i in oscleap:
+                ur.append(i[0])
+                wr.append(i[1])
+                 
+        if rep == "s":
+            
+            plt.title("U(r) frente a la coordenada radial: método de {}".format(metnam))
+            plt.xlabel("r (fm)")
+            plt.ylabel("U(r)")
+            urn = norm(ur, dx)
+            plt.plot(x,urn, color ="k", label="(n,l) = ({},{})". format(n, l))
+            plt.legend(loc="best", frameon = False)
+            plt.show()
+            
+            if sav == "s":
+                plt.savefig(input("Introduzca el nombre de la figura 1: "), dpi= 400)
+                
+            fig2 = plt.figure()
+            plt.title("R(r) frente a la coordenada radial: método de {}".format(metnam))
+            plt.xlabel("r (fm)")
+            plt.ylabel("R(r)")
+            rr = np.zeros(len(ur))
+            rr[1:] = urn[1:]/x[1:]
+            rrn = norm(rr, dx)
+            plt.plot(x[1:], rrn[1:], color="k", label="(n,l) = ({},{})". format(n, l))
+            plt.legend(loc="best", frameon = False)
+                 
+            if sav == "s":
+                plt.savefig(input("Introduzca el nombre de la figura 2: "), dpi= 400)
+
+##############################################################################
+##############################################################################
+############################################################
+############################################################
+#########################################
+#########################################
